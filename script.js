5cb727cf7e6c28f78edc9c4f6ad219bf0ae7a98e
WinterCricket <hodhodhod2003@gmail.com>
2020-02-10 22:54:39 -0600

    add js events to seats and counter

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const filmSelect = document.getElementById("film");
+
+const ticketPrice = +film.value;
+
+//functions
+function updateSelectedCount(){
+	const selectedSeats = document.querySelectorAll(".row .seat.selected");
+	const selectedSeatsCount = selectedSeats.length;
+	console.log(selectedSeatsCount);
+}
+
+//event
+container.addEventListener("click", (e) => {
+	if(e.target.classList.contains("seat") && !e.target.classList.contains("occupied")){
+		e.target.classList.toggle("selected");
+
+		updateSelectedCount();
+	}
+});
